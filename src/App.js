63c92531a2f7249dc99d7258fe4526e484778c99
Jasondanie <jasondanie@gmail.com>
2025-05-29 17:31:25 +0000

    Initial React app setup in Codespace

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <header>
+        <p>
+          Welkom bij mijn Codespace React App!
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
